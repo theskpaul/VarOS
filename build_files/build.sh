a0f43602573ccc4a233f4945e9dda03356f8499e
Sourav Kumar Paul <theskpaul@proton.me>
2026-01-18 10:22:41 +0530

    First Commit

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,13 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install -y "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
+
+dnf5 makecache --refresh -y
+dnf5 install -y alacritty bat clang neovim docker-cli containerd docker-buildx docker-compose docker-compose-switch thefuck podman-compose godot mono-devel zsh zed git toolbox zsh-autosuggestions zsh-syntax-highlighting gh  --skip-unavailable
+dnf5 install -y https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64
 
 # Use a COPR Example:
 #
