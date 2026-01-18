e63b2b03483c929b22e93cf4ce201db3ff6a33f8
Sourav Kumar Paul <theskpaul@proton.me>
2026-01-18 13:45:30 +0530

    Remove ProtonVPN

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,8 +5,6 @@ set -ouex pipefail
 ### Install packages
 dnf5 config-manager setopt terra.enabled=1
 
-dnf5 install -y "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
-dnf5 install -y proton-vpn-gnome-desktop --skip-broken --skip-unavailable
 dnf5 install -y alacritty bat clang neovim docker-cli containerd docker-buildx docker-compose docker-compose-switch thefuck podman-compose godot mono-devel zsh zed git toolbox zsh-autosuggestions zsh-syntax-highlighting gh code btop --skip-unavailable
 
 #### Example for enabling a System Unit File
