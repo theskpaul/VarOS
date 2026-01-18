493e280d1bfe68cbb7418ff99c6a2fce3840ff6f
Sourav Kumar Paul <theskpaul@proton.me>
2026-01-18 17:06:17 +0530

    New Packages added

--- Containerfile
+++ Containerfile
@@ -10,8 +10,9 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
+    /ctx/packages.sh \
     /ctx/build.sh
-    
+
 ### LINTING
 ## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,11 +2,6 @@
 
 set -ouex pipefail
 
-### Install packages
-dnf5 config-manager setopt terra.enabled=1
-
-dnf5 install -y alacritty bat clang neovim docker docker-cli containerd docker-buildx docker-compose docker-compose-switch thefuck podman-compose godot mono-devel zsh zed git toolbox zsh-autosuggestions zsh-syntax-highlighting gh code btop --skip-unavailable
-
-#### Example for enabling a System Unit File
+#### Enabling a System Unit File
 
 systemctl enable podman.socket
