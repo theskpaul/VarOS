3325880b9c7e8d1e3eb78640a85192caec071011
Sourav Kumar Paul <theskpaul@proton.me>
2026-01-18 17:08:58 +0530

    Missing Package Issue Fixed

---
+++ build_files/packages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+dnf5 config-manager setopt terra.enabled=1
+
+PACKAGES = "alacritty \
+            bat \
+            neovim \
+            gparted \
+            docker \
+            docker-cli \
+            containerd \
+            docker-buildx \
+            docker-compose \
+            docker-compose-switch \
+            thefuck podman-compose \
+            godot \
+            zsh \
+            zed \
+            git \
+            toolbox \
+            zsh-autosuggestions \
+            zsh-syntax-highlighting \
+            gh \
+            code \
+            fuse-btrfs \
+            android-tools \
+            android-file-transfer \
+            google-android-emoji-fonts \
+            memtest86+ \
+            smartmontools \
+            plasma-firewall \
+            ksyslog \
+            kgpg \
+            kcalc \
+            okular \
+            gwenview \
+            persepolis \
+            clamav \
+            clamtk \
+            btop"
+
+dnf5 install -y $PACKAGES --skip-unavailable
