01d62597b2ba44cb8eec741adb85a7d0ef0490d2
Varuos <var0@192.168.1.8>
2026-04-24 09:27:25 +0530

    Install Arattai

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -2,8 +2,12 @@
 
 set -ouex pipefail
 
+### Download package
+wget https://downloads.zohocdn.com/arattai-desktop/linux/arattai-1.0.8_x86_64.rpm -O /tmp/arattai.rpm
+
 ### Install packages
 dnf5 config-manager setopt rpmfusion-nonfree.enabled=1
+dnf5 install /tmp/arattai.rpm -y
 
 dnf5 remove -y libva-intel-media-driver
 
