7c366f3e119b362adc87ae9294a07cc1629029f4
Sourav Kumar Paul <theskpaul@proton.me>
2026-05-08 00:42:43 +0530

    Fix ProtonVPN post-install script

--- build_files/package_groups/60-additional-packages.sh
+++ build_files/package_groups/60-additional-packages.sh
@@ -2,11 +2,10 @@
 
 set -xeuo pipefail
 
-dnf5 upgrade -y
-
 dnf5 --setopt=install_weak_deps=False install -y \
     falkon
 
 dnf5 config-manager setopt protonvpn-fedora-stable.enabled=0
 dnf5 install --enable-repo="protonvpn-fedora-stable" -y \
+    --setopt=tsflags=noscripts \
     proton-vpn-gnome-desktop
