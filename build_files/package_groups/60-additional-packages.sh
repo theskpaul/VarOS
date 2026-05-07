10fb9b653ba3b60462e33c9b5672c5f28bee891f
Sourav Kumar Paul <theskpaul@proton.me>
2026-05-08 00:22:35 +0530

    Add ProtonVPN

--- build_files/package_groups/60-additional-packages.sh
+++ build_files/package_groups/60-additional-packages.sh
@@ -2,5 +2,11 @@
 
 set -xeuo pipefail
 
+dnf5 upgrade -y
+
 dnf5 --setopt=install_weak_deps=False install -y \
     falkon
+
+dnf5 config-manager setopt protonvpn-fedora-stable.enabled=0
+dnf5 install --enable-repo="protonvpn-fedora-stable" -y \
+    proton-vpn-gnome-desktop
