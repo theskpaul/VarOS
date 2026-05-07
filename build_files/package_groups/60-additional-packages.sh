ef5aa2a2bd041c984638a1d6f91ef018d3cc44a4
Sourav Kumar Paul <153661562+theskpaul@users.noreply.github.com>
2026-05-08 01:10:42 +0530

    ProtonVPN (#9)
    * Add image cleaning facility
    
    * Stop facilities that need cleanup
    
    * Add ProtonVPN
    
    * Fix ProtonVPN post-install script

--- build_files/package_groups/60-additional-packages.sh
+++ build_files/package_groups/60-additional-packages.sh
@@ -4,3 +4,8 @@ set -xeuo pipefail
 
 dnf5 --setopt=install_weak_deps=False install -y \
     falkon
+
+dnf5 config-manager setopt protonvpn-fedora-stable.enabled=0
+dnf5 install --enable-repo="protonvpn-fedora-stable" -y \
+    --setopt=tsflags=noscripts \
+    proton-vpn-gnome-desktop
--- build_files/sys-config.sh
+++ build_files/sys-config.sh
@@ -5,4 +5,4 @@ set -ouex pipefail
 # Daemons
 systemctl enable podman.socket
 systemctl enable docker.socket
-#
+systemctl enable me.proton.vpn.split_tunneling.service
