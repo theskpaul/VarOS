2f88a7f521f7cbbdb13cd8aea567cfcaa2c30341
Sourav Kumar Paul <theskpaul@proton.me>
2026-05-10 22:00:23 +0530

    Add bindfs support

--- build_files/package_groups/60-additional-packages.sh
+++ build_files/package_groups/60-additional-packages.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 
 dnf5 --setopt=install_weak_deps=False install -y \
     falkon
+    bindfs
 
 dnf5 config-manager setopt protonvpn-fedora-stable.enabled=0
 dnf5 --setopt=install_weak_deps=False install --enable-repo="protonvpn-fedora-stable" -y \
