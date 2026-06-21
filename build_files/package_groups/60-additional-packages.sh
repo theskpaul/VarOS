c5739bfe5e6bd87d652952f3cb129862226af1f7
Sourav Kumar Paul <153661562+theskpaul@users.noreply.github.com>
2026-06-21 15:10:02 +0530

    Disable brave-browser and protonvpn-fedora-stable repos

--- build_files/package_groups/60-additional-packages.sh
+++ build_files/package_groups/60-additional-packages.sh
@@ -8,6 +8,7 @@ dnf5 --setopt=install_weak_deps=False install -y \
     ptyxis \
     brave-browser
 
+dnf5 config-manager setopt brave-browser.enabled=0
 dnf5 config-manager setopt protonvpn-fedora-stable.enabled=0
 dnf5 --setopt=install_weak_deps=False install --enable-repo="protonvpn-fedora-stable" -y \
     --setopt=tsflags=noscripts \
