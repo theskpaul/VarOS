c6e884cc237318afc5256f584c6db5b049975cb4
Sourav Kumar Paul <153661562+theskpaul@users.noreply.github.com>
2026-06-21 15:09:10 +0530

    Replace Falkon with Brave Browser in package install

--- build_files/package_groups/60-additional-packages.sh
+++ build_files/package_groups/60-additional-packages.sh
@@ -5,8 +5,8 @@ IFS=$'\n\t'
 umask 022
 
 dnf5 --setopt=install_weak_deps=False install -y \
-    falkon \
-    ptyxis
+    ptyxis \
+    brave-browser
 
 dnf5 config-manager setopt protonvpn-fedora-stable.enabled=0
 dnf5 --setopt=install_weak_deps=False install --enable-repo="protonvpn-fedora-stable" -y \
